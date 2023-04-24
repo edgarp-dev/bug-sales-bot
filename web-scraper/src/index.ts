946b2c7f61136bd43b2aa06084291acef6f76786
edgarp-dev <edgarp.dev@gmail.com>
2023-04-24 08:46:28 -0600

    [UPDATE] Added basic project files

--- web-scraper/src/index.ts
+++ web-scraper/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello world :D');
+import cron from 'node-cron';
+
+const sayHello = async () => {
+  console.log('Hello world :D');
+};
+
+cron.schedule('* * * * *', async () => {
+  await sayHello();
+});
