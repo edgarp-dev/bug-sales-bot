d8102bf2a36f2216b9d988a6f1f5cef156075e0d
edgarp-dev <edgarp.dev@gmail.com>
2023-05-11 20:01:07 -0600

    [Added lambda files]

--- notifications-processor/src/app.ts
+++ notifications-processor/src/app.ts
@@ -1,8 +1,13 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
+import axios from 'axios';
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<any> => {
     try {
         console.log(event);
+        await axios.post('https://api.telegram.org/bot5848018748:AAEwAb31VJWc0e9GiAZONi4aVYrdkABdEeE/sendMessage', {
+            chat_id: '@promobugsdev',
+            text: 'Test message',
+        });
     } catch (err) {
         console.log(err);
     }
