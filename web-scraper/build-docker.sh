271fa56a45b66276e73b9ca13825e40d65c27aac
edgarp-dev <edgarp.dev@gmail.com>
2023-06-21 08:27:10 -0600

    [UPDATE] Fixed docker error and refactored deployment

--- web-scraper/build-docker.sh
+++ web-scraper/build-docker.sh
@@ -6,4 +6,4 @@ docker buildx use bug-sales-builder
 
 docker buildx inspect --bootstrap
 
-docker buildx build --platform linux/arm64/v8 -t edgarpdev/bug-sales-bot-web-scraper --push .
+docker buildx build --platform linux/arm64/v8,linux/amd64 -t edgarpdev/bug-sales-bot-web-scraper --push .
