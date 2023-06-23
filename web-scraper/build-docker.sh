bc6ef687536b73adb7d5ee381bf620d7b9e088c4
edgarp-dev <edgarp.dev@gmail.com>
2023-06-23 08:29:35 -0600

    [UPDATE] Refactored command

--- web-scraper/build-docker.sh
+++ web-scraper/build-docker.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-docker buildx create --name bug-sales-builder
+docker buildx create --name bug-sales-builder --driver-opt network=host --buildkitd-flags '--allow-insecure-entitlement network.host'
 
 docker buildx use bug-sales-builder
 
 docker buildx inspect --bootstrap
 
-docker buildx build --network=host --platform linux/amd64 -t edgarpdev/bug-sales-bot-web-scraper --push .
+docker buildx build --platform linux/amd64 -t edgarpdev/bug-sales-bot-web-scraper --push .
