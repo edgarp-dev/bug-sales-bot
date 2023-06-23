5577e6e2bad2c1eabcb1b0d9c436b1881c6d93e6
edgarp-dev <edgarp.dev@gmail.com>
2023-06-23 08:23:29 -0600

    [UPDATE] Added network flag to docker command

--- web-scraper/build-docker.sh
+++ web-scraper/build-docker.sh
@@ -6,4 +6,4 @@ docker buildx use bug-sales-builder
 
 docker buildx inspect --bootstrap
 
-docker buildx build --platform linux/amd64 -t edgarpdev/bug-sales-bot-web-scraper --push .
+docker buildx build --network=host --platform linux/amd64 -t edgarpdev/bug-sales-bot-web-scraper --push .
