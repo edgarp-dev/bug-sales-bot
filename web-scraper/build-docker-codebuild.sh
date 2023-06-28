437051f528644686bca36e22efd436e5c35db96b
edgarp-dev <edgarp.dev@gmail.com>
2023-06-28 13:10:38 -0600

    [UPDATE] Added env and args

--- web-scraper/build-docker-codebuild.sh
+++ web-scraper/build-docker-codebuild.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-docker build --build-arg AWS_REGION=$REGION --build-arg AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID --build-arg AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY  -t edgarpdev/bug-sales-bot-web-scraper .
+docker build --build-arg ARG_AWS_REGION=$REGION --build-arg ARG_AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID --build-arg ARG_AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY  -t edgarpdev/bug-sales-bot-web-scraper .
 
 docker push edgarpdev/bug-sales-bot-web-scraper
