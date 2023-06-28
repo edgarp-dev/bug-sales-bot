a1a578022ae8bdac6e18d48dc452d24ec02dd6d2
edgarp-dev <edgarp.dev@gmail.com>
2023-06-28 12:07:04 -0600

    [UPDATE] Reverted docker image name

--- web-scraper/build-docker-codebuild.sh
+++ web-scraper/build-docker-codebuild.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-docker build --build-arg LOCALHOST=false --build-arg AWS_REGION=$REGION --build-arg AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID --build-arg AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY  -t edgarpdev/bug-sales-bot-web-scraper-$ENV .
+docker build --build-arg LOCALHOST=false --build-arg AWS_REGION=$REGION --build-arg AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID --build-arg AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY  -t edgarpdev/bug-sales-bot-web-scraper .
 
-docker push edgarpdev/bug-sales-bot-web-scraper-$ENV
+docker push edgarpdev/bug-sales-bot-web-scraper
--- web-scraper/src/index.ts
+++ web-scraper/src/index.ts
@@ -41,7 +41,7 @@ const isLocalhost = process.env.LOCALHOST === 'true';
 
 cron.schedule('* * * * *', async () => {
   try {
-    console.log('v0.5.19');
+    console.log('v0.5.20');
     console.log('Requesting sales');
     const bugSales = await requestBugSales();
     if (bugSales) {
