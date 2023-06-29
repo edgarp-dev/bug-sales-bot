d82701f4f720344cbca5edfc8ea3c498403b02f0
edgarp-dev <edgarp.dev@gmail.com>
2023-06-29 08:18:27 -0600

    [UPDATE] Debug

--- web-scraper/build-docker-codebuild.sh
+++ web-scraper/build-docker-codebuild.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
-docker build --build-arg ARG_AWS_REGION=$REGION --build-arg ARG_AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID --build-arg ARG_AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY  -t edgarpdev/bug-sales-bot-web-scraper .
+docker build --no-cache \
+    --build-arg ARG_AWS_REGION=$REGION \
+    --build-arg ARG_AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID \
+    --build-arg ARG_AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY \
+    -t edgarpdev/bug-sales-bot-web-scraper .
 
 docker push edgarpdev/bug-sales-bot-web-scraper
