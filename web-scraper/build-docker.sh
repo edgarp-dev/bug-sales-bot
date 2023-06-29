e89cd424da1770c391ccd9228d11cc57e53d4d88
edgarp-dev <edgarp.dev@gmail.com>
2023-06-29 09:44:52 -0600

    [UPDATE] Upgraded version and fixed local docker build script

--- web-scraper/build-docker.sh
+++ web-scraper/build-docker.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 
+source .env
+
 docker buildx create --name bug-sales-builder
 
 docker buildx use bug-sales-builder
 
 docker buildx inspect --bootstrap
 
-docker buildx build --platform linux/amd64 -t edgarpdev/bug-sales-bot-web-scraper --push .
+docker buildx build --platform linux/amd64 \
+    --build-arg ARG_AWS_REGION=$AWS_REGION \
+    --build-arg ARG_AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+    --build-arg ARG_AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+    -t edgarpdev/bug-sales-bot-web-scraper --push .
--- web-scraper/src/index.ts
+++ web-scraper/src/index.ts
@@ -41,7 +41,7 @@ const isLocalhost = process.env.LOCALHOST === 'true';
 
 cron.schedule('* * * * *', async () => {
   try {
-    console.log('v0.5.25');
+    console.log('v0.5.27');
     console.log('Requesting sales');
     const bugSales = await requestBugSales();
     if (bugSales) {
