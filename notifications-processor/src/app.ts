91fb9c7e217bd23d49846e53f264e09743bdfadd
edgarp-dev <edgarp.dev@gmail.com>
2023-06-30 17:40:44 -0600

    [UPDATE] Refactored notifications handler

--- notifications-processor/src/app.ts
+++ notifications-processor/src/app.ts
@@ -3,9 +3,6 @@ import axios from 'axios';
 
 export const lambdaHandler = async (event: SNSEvent): Promise<void> => {
     try {
-        const telegramBotToken = `bot${process.env.TELEGRAM_BOT_TOKEN}`;
-        const telegramPostUrl = `https://api.telegram.org/${telegramBotToken}/sendPhoto`;
-        console.log(event.Records[0].Sns.MessageAttributes);
         const {
             title: titleAttribute,
             url: urlAttribute,
@@ -15,15 +12,16 @@ export const lambdaHandler = async (event: SNSEvent): Promise<void> => {
         const title = titleAttribute.Value;
         const url = urlAttribute.Value;
         const imageUrl = imageUrlAttribute.Value;
+        const captionHtml = `<b>${title}</b>\u000a<a href="${url}">${url}</a>`;
 
-        // const message = `*${title}*  [${url}](${url})  [ ](${imageUrl}) image`;
-        const message = `<b>${title}</b> <a href="${url}">${url}</a>`;
+        const telegramBotToken = `bot${process.env.TELEGRAM_BOT_TOKEN}`;
+        const telegramPostUrl = `https://api.telegram.org/${telegramBotToken}/sendPhoto`;
 
         const response = await axios.post(telegramPostUrl, {
             chat_id: '@promobugsdev',
             photo: imageUrl,
             parse_mode: 'HTML',
-            caption: 'test',
+            caption: captionHtml,
         });
 
         console.log(response);
